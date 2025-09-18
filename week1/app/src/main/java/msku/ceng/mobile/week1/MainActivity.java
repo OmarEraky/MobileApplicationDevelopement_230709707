604d4892dfd20443502a0d3e852f2b4e9af6ea72
omar_mohamed_eleraky <omarhassanein@posta.mu.edu.tr>
2025-09-18 15:21:14 +0300

    repush

--- week1/app/src/main/java/msku/ceng/mobile/week1/MainActivity.java
+++ week1/app/src/main/java/msku/ceng/mobile/week1/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+/*this is the first app*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
