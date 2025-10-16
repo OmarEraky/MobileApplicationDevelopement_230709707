0391ed0e224842234bfee53c0eceeab9c1c41616
omar_mohamed_eleraky <omarhassanein@posta.mu.edu.tr>
2025-10-16 11:12:34 +0300

    Week4 part2

--- week4/app/src/main/java/msku/ceng/week4/MainActivity.java
+++ week4/app/src/main/java/msku/ceng/week4/MainActivity.java
@@ -3,6 +3,11 @@ package msku.ceng.week4;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +18,32 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnGo;
+    EditText txturl;
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnGo = findViewById(R.id.btnGo);
+        txturl = findViewById(R.id.txtUrl);
+        webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl("https://"+txturl.getText().toString());
+            }
+        });
+        if(getIntent() != null && getIntent().getData() != null){
+            txturl.setText(getIntent().getData().toString());
+            webView.loadUrl(getIntent().getData().toString());
+        }
     }
 
     @Override
